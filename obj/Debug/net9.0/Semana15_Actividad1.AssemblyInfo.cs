207148bd3a38867e08e403eed13befed8bb1523d
Lesther España <lcespana@correo.url.edu.gt>
2025-04-25 19:04:34 -0600

    commit actividad 1 semana 15

--- obj/Debug/net9.0/Semana15_Actividad1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Semana15_Actividad1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semana15_Actividad1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+270e1a7c2833234e1709474eaccce367ecbb9687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f299cebfad9287dc75a90f74328c25ef6bdf95bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semana15_Actividad1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semana15_Actividad1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
